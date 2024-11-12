50557ea61ad4dae3c3fcd6bc3cf07cb58ffb70e6
TomatoHB <binhuang98@live.com>
2024-11-12 15:42:05 +0800

    feat: 初步添加分隔面板

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,34 +1,39 @@
-import React from 'react';
-import { RouteObject } from 'react-router-dom';
-import Home from 'pages/Home';
-import FormItemSection from 'pages/form/FormItemSection';
-import FormListSection from 'pages/form/FormListSection';
-import TableSection from 'pages/table';
-import ComingSoon from '@/components/ComingSoon';
+import React from "react";
+import { RouteObject } from "react-router-dom";
+import Home from "pages/Home";
+import FormItemSection from "pages/form/FormItemSection";
+import FormListSection from "pages/form/FormListSection";
+import TableSection from "pages/table";
+import SplitterSection from "pages/splitter";
+import ComingSoon from "@/components/ComingSoon";
 
 export const contentRoutes: RouteObject[] = [
   {
-    path: '/form',
+    path: "/form",
     children: [
       {
-        path: '/form/formItemSection',
+        path: "/form/formItemSection",
         element: <FormItemSection />,
       },
       {
-        path: '/form/formListSection',
+        path: "/form/formListSection",
         element: <FormListSection />,
       },
     ],
   },
   {
-    path: '/table',
+    path: "/table",
     element: <TableSection />,
+  },
+  {
+    path: "/splitter",
+    element: <SplitterSection />,
   },
 ];
 
 const routes: RouteObject[] = [
   {
-    path: '/*',
+    path: "/*",
     element: <Home />,
   },
 ];
